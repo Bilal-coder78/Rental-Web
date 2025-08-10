d1d54e2cf18013b91f18c36b511a2d0143cc7522
Bilal-coder78 <bilalsikander87@gmail.com>
2025-08-10 21:10:34 +0500

    nav1 completed

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Nav.css"
 import { CiSearch } from "react-icons/ci";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -6,8 +6,15 @@ import { CgProfile } from "react-icons/cg";
 import logo from "../../assets/p.jpg"
 
 function Nav() {
+    const [visible,setvisible] = useState(false)
   return (
     <div id='Nav'>
+        {visible?<div className="hamburger">
+            <div className="ham1">Login</div>
+            <div className="ham1">Sign Up</div>
+            <div className="ham1">List your home</div>
+            <div className="ham1">Help center</div>
+        </div>:null}
         <div className="nav1">
             <div className="logo">
                 <img src={logo} alt="" width="50px"/>
@@ -20,7 +27,9 @@ function Nav() {
             <div className="ham">
                 <button className='btn1'>List Your Home</button>
                 <button className='btn2'>
-                <GiHamburgerMenu id='svg1'/>
+                <GiHamburgerMenu id='svg1' onClick={()=>{
+                    setvisible(prev=>!prev)
+                }}/>
                 <CgProfile id='svg2'/>
                 </button>
             </div>
